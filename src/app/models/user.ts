b500e78720d3c4aaa1453e5f2acebf80af031d0f
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-01-13 18:03:51 +0600

    Updated

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,1 +1,39 @@
-const userSchema = new Schema({});
+import { model, Schema } from "mongoose";
+import { TUser } from "../interfaces/user";
+
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "student", "faculty"],
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["in-progress", "blocked"],
+      // default: "in-progress"
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = model<TUser>("User", userSchema);
