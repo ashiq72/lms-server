5181416a127074856c69f521c611d3f196649d0c
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-08-11 05:51:46 +0600

    zod validation added in route

--- src/app/routes/user.ts
+++ src/app/routes/user.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { UserController } from "../controllers/user";
+import validateRequest from "../middlewares/validateRequest";
+import { createStudentValidationSchema } from "../zod/student.validation";
 const router = express.Router();
 
-router.post("/create-student", UserController.createStudent);
+router.post(
+  "/create-student",
+  validateRequest(createStudentValidationSchema),
+  UserController.createStudent
+);
 
 export const UserRouter = router;
--- src/app/zod/student.validation.ts
+++ src/app/zod/student.validation.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-const userNameSchema = z.object({
+const userNameValidationSchema = z.object({
   firstName: z
     .string()
     .min(1)
@@ -7,45 +7,46 @@ const userNameSchema = z.object({
     .refine((value) => /^[A-Z]/.test(value), {
       message: "First Name must start with a capital letter",
     }),
-  middleName: z.string().optional().or(z.literal("")), // Allow empty string
-  lastName: z.string().min(1).max(20),
+  middleName: z.string(),
+  lastName: z.string(),
 });
 
-const guardianSchema = z.object({
-  fatherName: z.string().min(1),
-  fatherOccupation: z.string().min(1),
-  fatherContactNo: z.string().min(1),
-  motherName: z.string().min(1),
-  motherOccupation: z.string().min(1),
-  motherContactNo: z.string().min(1),
+const guardianValidationSchema = z.object({
+  fatherName: z.string(),
+  fatherOccupation: z.string(),
+  fatherContactNo: z.string(),
+  motherName: z.string(),
+  motherOccupation: z.string(),
+  motherContactNo: z.string(),
 });
 
-const localGuardianSchema = z.object({
-  name: z.string().min(1),
-  occupation: z.string().min(1),
-  contactNo: z.string().min(1),
-  address: z.string().min(1),
+const localGuardianValidationSchema = z.object({
+  name: z.string(),
+  occupation: z.string(),
+  contactNo: z.string(),
+  address: z.string(),
 });
 
-export const studentValidationSchema = z.object({
-  id: z.string().min(1),
-  password: z.string().min(1).max(20),
-  name: userNameSchema,
-  gender: z.enum(["male", "female", "other"]),
-  dateOfBirth: z.string().optional(), // Make optional
-  email: z.string().email(),
-  contactNo: z.string().min(1),
-  emergencyContactNo: z.string().min(1),
-  bloodGroup: z
-    .enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
-    .optional(), // Optional
-  presentAddress: z.string().min(1),
-  permanentAddress: z.string().min(1),
-  guardian: guardianSchema,
-  localGuardian: localGuardianSchema,
-  profileImg: z.string().optional(), // Make optional
-  isActive: z.enum(["active", "blocked"]).default("active"),
-  isDeleted: z.boolean().optional().default(false),
+export const createStudentValidationSchema = z.object({
+  body: z.object({
+    password: z.string().max(20),
+    student: z.object({
+      name: userNameValidationSchema,
+      gender: z.enum(["male", "female", "other"]),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email(),
+      contactNo: z.string(),
+      emergencyContactNo: z.string(),
+      bloodGroup: z.enum(["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]),
+      presentAddress: z.string(),
+      permanentAddress: z.string(),
+      guardian: guardianValidationSchema,
+      localGuardian: localGuardianValidationSchema,
+      profileImg: z.string(),
+    }),
+  }),
 });
 
-export default studentValidationSchema;
+export const studentValidations = {
+  createStudentValidationSchema,
+};
