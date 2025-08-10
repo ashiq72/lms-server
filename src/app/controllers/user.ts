65d154794776e31349808a9b47f21e2f5942b93e
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-08-10 08:12:43 +0600

    sendResponse added in user controller

--- src/app/controllers/user.ts
+++ src/app/controllers/user.ts
@@ -1,15 +1,17 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
 import { UserServices } from "../services/user";
 import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
 
 const createStudent = catchAsync(async (req, res) => {
   const { password, student: studentData } = req.body;
 
   const result = await UserServices.createStudentIntoDB(password, studentData);
 
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
-    message: "Student created successfully",
+    message: "Student Create Successfully",
     data: result,
   });
 });
