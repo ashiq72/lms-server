2bf2149689c0e128b18f74f1321ce262fc8a3159
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-08-11 20:18:57 +0600

    Updated academic semester api validation added

--- src/app/services/academicSemester.ts
+++ src/app/services/academicSemester.ts
@@ -2,6 +2,20 @@ import { TAcademicSemester } from "../interfaces/academicSemester";
 import { AcademicSemester } from "../models/academicSemester";
 
 const createAcademicSemesterIntoDb = async (payload: TAcademicSemester) => {
+  type TAcademicSemesterNameCodeMapper = {
+    [key: string]: string;
+  };
+
+  const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+    Autumn: "01",
+    Summar: "02",
+    Fall: "03",
+  };
+
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error("Invalid Semester Code");
+  }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
