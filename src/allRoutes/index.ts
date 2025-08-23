9bd2ec2c909b66f0cb6ae65103f91be1be4959b4
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-08-23 23:41:23 +0600

    Academic faculty  api added

--- src/allRoutes/index.ts
+++ src/allRoutes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { UserRouter } from "../app/routes/user";
 import { StudentRoutes } from "../app/routes/student";
 import { AcademicSemesterRoutes } from "../app/routes/academicSemester";
+import { AcademicFacultyRoutes } from "../app/routes/acdemicFaculty";
 
 const router = Router();
 
 router.use("/users", UserRouter);
 router.use("/students", StudentRoutes);
 router.use("/academic-semester", AcademicSemesterRoutes);
+router.use("/academic-faculty", AcademicFacultyRoutes);
 
 export default router;
