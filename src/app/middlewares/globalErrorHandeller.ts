b8aa5ac37ac6a5b3aa53e3e038a4d4df66c97eb0
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-08-31 19:33:08 +0600

    convert Zod Error

--- src/app/middlewares/globalErrorHandeller.ts
+++ src/app/middlewares/globalErrorHandeller.ts
@@ -1,4 +1,7 @@
 import { Request, Response, NextFunction } from "express";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSources } from "../../interface/error";
+import config from "../../config";
 
 const globalErrorHandler = (
   err: any,
@@ -6,14 +9,53 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Internal Server Error";
+
+  let errorSources: TErrorSources = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+
+    const statusCode = 400;
+    return {
+      statusCode,
+      message: "Validation Error",
+      errorSources,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
 
   res.status(statusCode).json({
     success: false,
     message,
-    error: err, // optional: return stack trace or validation errors
+    errorSources,
+    stack: config.NODE_ENV == "development" ? err?.stack : null,
   });
 };
 
 export default globalErrorHandler;
+
+// pattern
+// success
+// message
+// errorSources: [
+//   path:
+//   stack
+// ]
