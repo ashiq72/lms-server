5247a2feb09f3ab89c4dbff5b1321f302a1b8538
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-09-15 20:19:32 +0600

    added file stucture uaer, atudent, academic semister API

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,66 @@
+import { Types } from "mongoose";
+import { Model } from "mongoose";
+
+// 1. Create an interface representing a document in MongoDB.
+export type TUserName = {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+};
+
+export type TGuardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type TLocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type TStudent = {
+  id: string;
+  user: Types.ObjectId;
+  password: string;
+  name: TUserName;
+  gender: "male" | "female" | "other";
+  dateOfBirth?: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: TGuardian;
+  localGuardian: TLocalGuardian;
+  profileImg?: string;
+  admissionSemester: Types.ObjectId;
+  // academicDepartment: Types.ObjectId;
+  isDeleted: boolean;
+};
+
+//for creating static
+
+export interface StudentModel extends Model<TStudent> {
+  isUserExists(id: string): Promise<TStudent | null>;
+}
+
+// For creating Staic
+
+// For creating instance
+
+// export type StudentMethods = {
+//   isUserExists(id: string): Promise<TStudent | null>;
+// };
+
+// export type StudentModel = Model<
+//   TStudent,
+//   Record<string, never>,
+//   StudentMethods
+// >;
