6a3206eb6d8f10eb43f6348da3bd55c153f72303
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-10-01 10:55:57 +0600

    auth validation added in auth route

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -5,6 +5,10 @@ import { AuthControllers } from "../auth/auth.controller";
 
 const router = express.Router();
 
-router.post("/login", AuthControllers.loginUser);
+router.post(
+  "/login",
+  validateRequest(AuthValidation.loginValidationSchema),
+  AuthControllers.loginUser
+);
 
 export const AuthRoutes = router;
