cf1b80ca65a8cb296279445e606b52e2a58868a5
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-10-07 21:07:06 +0600

    change-password route added

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -10,5 +10,10 @@ router.post(
   validateRequest(AuthValidation.loginValidationSchema),
   AuthControllers.loginUser
 );
+router.post(
+  "/change-password",
+  validateRequest(AuthValidation.loginValidationSchema),
+  AuthControllers.loginUser
+);
 
 export const AuthRoutes = router;
