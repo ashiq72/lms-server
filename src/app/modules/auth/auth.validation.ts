4ef4e19918f30f9636d071db83ea187bf4bf38d2
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-10-13 19:28:19 +0600

    forget-password validation added

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -19,9 +19,15 @@ const refreshTokenValidationSchema = z.object({
     refreshToken: z.string({ required_error: "Refresh token is required" }),
   }),
 });
+const forgetPasswordValidationSchema = z.object({
+  body: z.object({
+    id: z.string({ required_error: "User id is required" }),
+  }),
+});
 
 export const AuthValidation = {
   loginValidationSchema,
   changePasswordValidationSchema,
   refreshTokenValidationSchema,
+  forgetPasswordValidationSchema,
 };
