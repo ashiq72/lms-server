52f3356a7951db5d99df7ba6851dca28739798bf
Md ashiquzzaman <ahmedashik18k@gmail.com>
2025-10-13 19:32:41 +0600

    forgetPassword controller added link in route

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -26,7 +26,7 @@ router.post(
 router.post(
   "/forget-password",
   validateRequest(AuthValidation.forgetPasswordValidationSchema),
-  AuthControllers.refreshToken
+  AuthControllers.forgetPassword
 );
 
 export const AuthRoutes = router;
